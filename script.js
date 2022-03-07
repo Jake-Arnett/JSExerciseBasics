8951a3e0ec56ed0e2ff86e28633bdead76e8253e
“Jake-Arnett” <Jake.Arnett@gmail.com>
2022-03-06 22:50:53 -0500

    created JS Basics Exercise file

---
+++ script.js
@@ -0,0 +1,57 @@
+// 1. Use a console log to have your favorite number display in the console/terminal
+    console.log(182);
+
+
+// 2. Use a comment to type out the 5 main data types (AKA Primitives)
+    //  Numbers, Strings, Booleans, null and undefined
+
+
+// 3. Use a comment to type out the 6 operator symbols
+    // +  -  *  /  **  %  
+
+
+// 4. Use modulus to get a remainder of 1 from the number 5. (There are two answers for this one. Please provide both. Also, please type them both out. Do not just type out the two numbers)
+    console.log(5 % 2);
+
+
+// 5. Using single quotes at the start and end of the string have the words I'M MARY POPPINS, Y'ALL! display in the terminal/console (Must use a console log for this one.)
+    console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+
+// 6. Using Concatenation combine the following strings into one string. First string: "Hawk". Second string: "eye".
+    console.log("Hawk" + "eye");
+
+
+// 7. Create a string for your first name and also your last name. Use the length property to add them together (Answer should be a number)
+    console.log("Jake".length + "Arnett".length);
+
+
+// 8. Use index on the following string to select the lowercase "e" at the end of the string. "Avengers: Endgame". (Use a console log to display it in the console/terminal)
+    console.log("Avengers: Endgame"[16]);
+
+
+// BONUS
+
+// 1. Create a string that displays only a backslash when it is displayed in the console/terminal
+    console.log("\\")
+
+
+// 2. Use the following numbers in the order shown to get a result of the 616. Numbers are 5, 4, and 9 (HINT: Exponentiation)
+    console.log(5**4 - 9);
+
+
+// 3. Use the following strings in order "I", "Am", "Iron", and "Man" to display a result of Zero in the console/terminal using the length property. (NOTE: There are multiple answers. For an extra bonus try to use modulo).
+    console.log("I".length % "Am".length - "Iron".length + "Man".length)
+
+
+// 4. Using Index on each of the following strings, have AVENGERS display vertically in the console/terminal (Strings: "CAP" "VISION" "OKOYE" "NICK FURY" "GROOT" "DOCTOR STRANGE" "THOR" "STAR-LORD"). (NOTE: Need to create a separate console log for each string).
+    console.log("CAP"[1] + "VISION"[0] + "OKOYE"[4] + "NICK FURY"[0] + "GROOT"[0] + "DOCTOR STRANGE"[13] + "THOR"[3] + "STAR-LORD"[0]);
+
+
+// 5. Create the following variables with the following values m=25 and c=4. Create a variable with the name of u and set the value for it by adding m and c together (NOTE: Must add actual variables NOT just 25 + 4 when setting value for the u variable).
+    let m = 25
+    let c = 4
+    let u = m + c
+    console.log(u); // 29
+
+
